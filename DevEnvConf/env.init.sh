55b7f1a2d8a8c5ca205998fe26695af9ed56e26b
sbxjoy <lx_kkudng@163.com>
2013-07-29 23:06:31 +0800

    修改env.init.sh 自动创建bin目录

--- DevEnvConf/env.init.sh
+++ DevEnvConf/env.init.sh
@@ -1,39 +1,35 @@
 #!/bin/sh
+
+#脚本初始化
 LANGUAGE='utf-8'
 this_file=`pwd`"/"$0
 project_home=`dirname $this_file`
 home=`echo ~`
 . $project_home/utils.sh
 
-dirs="$home/backup $home/backup/vim $home/bin"
+#创建一些目录
+#backup, backup/vim vim的backup目录
+#bin 当前用户的bin目录，放一些需要用的工具
+dirs="$home/backup $home/backup/vim"
 for dir in $dirs
 do
     if [ -a $dir ]
     then
-        cecho "$dir already exist!" $yellow
+        cecho "$dir 已存在！" $green
     else
         mkdir $dir
     fi
 done
 
-if ([ -L $home/.vim ]) || ([ -e $home/.vim ])
-then
-    confirm " ~/.vim already exist! replace?"
-    if [ $? == '1' ]
-    then
-        ln -sfn $project_home/vim $home/.vim
-    else
-        cecho "do not create ~/.vim" $red
-    fi
-fi
+#vim 相关的link
+#vim 目录
+link_path $project_home/vim $home/.vim
+#vimrc
+link_path $project_home/vim/_vimrc $home/.vimrc
 
-if ([ -L $home/.vimrc ]) || ([ -e $home/.vimrc ])
+#bin 目录的创建和检查
+link_path $project_home/bin $home/bin
+if [[ $PATH != *$home* ]]
 then
-    confirm " ~/.vimrc already exist! replace?"
-    if [ $? == '1' ]
-    then
-        ln -sfn $project_home/vim/_vimrc $home/.vimrc
-    else
-        cecho "do not create ~/.vimrc" $red
-    fi
+    cecho '$PATH 中不包含 '"$home/bin 请手动添加" $red
 fi
