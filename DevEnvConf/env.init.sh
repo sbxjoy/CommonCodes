06bb83eb3e2b84f6f1a5efa1c689aeb3c15e6884
lvxiang <lvxiang@dz01v.opend.corp.qihoo.net>
2014-05-23 14:27:04 +0800

    添加crontab 自动更新机制

--- DevEnvConf/env.init.sh
+++ DevEnvConf/env.init.sh
@@ -3,6 +3,7 @@
 LANGUAGE='utf-8'
 this_file=`pwd`"/"$0
 project_home=`dirname $this_file`
+echo $project_home
 . $project_home/utils.sh
 
 #创建一些目录
@@ -31,5 +32,29 @@ if [[ $PATH != *$HOME/bin* ]]
 then
     cecho '$PATH 中不包含 '"$HOME/bin 请手动添加" $red
 fi
-
 #添加bash环境参数
+# TODO
+
+crontab -l > /tmp/curr.cron
+cnt=`cat /tmp/curr.cron | grep $project_home | wc -l`
+if test $cnt -eq 0
+then
+    cecho "crontab 更新脚本不存在！" $yellow
+    str="0 * * * * cd $project_home; git pull >> /dev/null 2>&1"
+    echo "$str" >> /tmp/curr.cron
+    crontab /tmp/curr.cron
+    cecho "crontab 更新脚本已经添加！" $green
+else
+    confirm " crontab 更新脚本已存在，替换吗？"
+    if [ $? == '1' ] 
+    then
+        cat /tmp/curr.cron | grep -v $project_home > /tmp/dest.cron
+        str="0 * * * * cd $project_home; git pull >> /dev/null 2>&1"
+        echo "$str" >> /tmp/dest.cron
+        crontab /tmp/dest.cron
+        rm -f /tmp/dest.cron
+    else
+        cecho "未处理更新脚本" $yellow
+    fi
+fi
+rm -f /tmp/curr.cron
